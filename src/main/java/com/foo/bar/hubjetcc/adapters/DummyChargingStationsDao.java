6835d4a1cac32743ebef407f0c3f51c90748d847
jmilkiewicz <jmilkiewicz@gmail.com>
2019-01-16 18:19:49 +0100

    making test pass

--- src/main/java/com/foo/bar/hubjetcc/adapters/DummyChargingStationsDao.java
+++ src/main/java/com/foo/bar/hubjetcc/adapters/DummyChargingStationsDao.java
@@ -3,10 +3,15 @@ package com.foo.bar.hubjetcc.adapters;
 import com.foo.bar.hubjetcc.model.ChargingStation;
 import com.foo.bar.hubjetcc.ports.ChargingStationDao;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 public class DummyChargingStationsDao implements ChargingStationDao {
+    private final ConcurrentMap<String, ChargingStation> storage = new ConcurrentHashMap<>();
 
     @Override
     public UpsertResult upsertChargingStation(ChargingStation chargingStation) {
-        return null;
+        storage.put(chargingStation.getGuid(), chargingStation);
+        return UpsertResult.CREATED;
     }
 }
