fe323093afd808cffde9cdc4ed05d7365871c7be
jmilkiewicz <jmilkiewicz@gmail.com>
2019-01-16 18:09:47 +0100

    refactor test

--- src/test/java/com/foo/bar/hubjetcc/usecases/CreateChargingStationTest.java
+++ src/test/java/com/foo/bar/hubjetcc/usecases/CreateChargingStationTest.java
@@ -3,40 +3,39 @@ package com.foo.bar.hubjetcc.usecases;
 import com.foo.bar.hubjetcc.model.ChargingStation;
 import com.foo.bar.hubjetcc.ports.ChargingStationDao;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.when;
 
 
-@RunWith(MockitoJUnitRunner.class)
 public class CreateChargingStationTest {
-    @Mock
-    private ChargingStationDao mockedChargingStationDao;
+
+    private ChargingStationDao mockedChargingStationDao = Mockito.mock(ChargingStationDao.class);
+    private CreateChargingStation sut = new CreateChargingStation(mockedChargingStationDao);
 
     private CharginStationRequest someChargingStationRequest = new CharginStationRequest("someId", 13.413215, 52.521918, "10178");
 
     @Test
     public void shallAddNewBrandingChargingStation() {
-        CreateChargingStation sut = new CreateChargingStation(mockedChargingStationDao);
-        when(mockedChargingStationDao.upsertChargingStation(Mockito.any(ChargingStation.class))).thenReturn(ChargingStationDao.UpsertResult.CREATED);
+        recordUpsertWith(ChargingStationDao.UpsertResult.CREATED);
         ServiceResponse serviceResponse = sut.addChargingStation(someChargingStationRequest);
 
-
-        assertThat(serviceResponse, is(new ServiceResponse.EntityCreatedResponse("chargingStation", someChargingStationRequest.getGuid())));
+        ServiceResponse.EntityCreatedResponse expected = new ServiceResponse.EntityCreatedResponse("chargingStation", someChargingStationRequest.getGuid());
+        assertThat(serviceResponse, is(expected));
     }
 
     @Test
     public void shallUpdateExistingChargingStation() {
-        CreateChargingStation sut = new CreateChargingStation(mockedChargingStationDao);
-        when(mockedChargingStationDao.upsertChargingStation(Mockito.any(ChargingStation.class))).thenReturn(ChargingStationDao.UpsertResult.UPDATED);
+        recordUpsertWith(ChargingStationDao.UpsertResult.UPDATED);
         ServiceResponse serviceResponse = sut.addChargingStation(someChargingStationRequest);
 
         assertThat(serviceResponse, is(ServiceResponse.OkResponse.Empty));
+    }
+
+    private void recordUpsertWith(ChargingStationDao.UpsertResult upsertResult) {
+        when(mockedChargingStationDao.upsertChargingStation(Mockito.any(ChargingStation.class))).thenReturn(upsertResult);
     }
 
 }
