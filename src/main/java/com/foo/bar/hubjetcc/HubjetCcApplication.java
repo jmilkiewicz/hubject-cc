3108a93826e458d7df1608db4f591bce40e8e875
jmilkiewicz <jmilkiewicz@gmail.com>
2019-01-16 16:38:09 +0100

    spring intializr generated project

---
+++ src/main/java/com/foo/bar/hubjetcc/HubjetCcApplication.java
@@ -0,0 +1,14 @@
+package com.foo.bar.hubjetcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubjetCcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubjetCcApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/foo/bar/hubjetcc/HubjetCcApplicationTests.java
@@ -0,0 +1,17 @@
+package com.foo.bar.hubjetcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HubjetCcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
